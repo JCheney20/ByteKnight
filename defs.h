0b945322d367023e431ec83ab0fcf74a47e41ce9
Justin Cheney <4323819@myuwc.ac.za>
2025-05-08 18:34:50 +0200

    init: Start of the C based Chess Engine project

---
+++ defs.h
@@ -0,0 +1,9 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+typedef unsigned long long U64;
+
+#define  NAME "ByteKnight 1.0"
+#define BRD_SQ_NUM 120
+
+#endif // !DEFS_H
---
+++ makefile
@@ -0,0 +1,2 @@
+all:
+	gcc ByteKnight.c -o ByteKnight
